56eb0c9e8e14b65b2fcd4001c7f76efc85753415
mamawski01 <72120572+mamawski01@users.noreply.github.com>
2025-01-08 09:36:58 +0800

    k

--- b/src/routes/routes.js
+++ b/src/routes/routes.js
@@ -11,14 +11,14 @@ export const routes = express.Router();
 const userImgFolName = "userImgFol";
 const userImgImgName = "userImg";
 
+const defMethods = ["GetAll", "GetOne"];
+
 export const urlArr = [
   // registryUser
-  { url: "/registryUserBEGetAll", model: RegistryUserModel },
-  { url: "/registryUserBEGetOne/:id", model: RegistryUserModel },
-  { url: "/registryUserBEPostCustom", model: RegistryUserModel },
-  //confirmUser
-  { url: "/confirmUserBEGetAll", model: ConfirmedUserModel },
+  { url: "/registryUserBE", model: RegistryUserModel },
 ];
+
+// i want to avoid repeating code more further, i created defMethods with string inside to be added to the url, it will serve as a complete url to do task assigned to it.
 
 urlArr.forEach((item) => {
   if (item.url.includes("GetAll")) {
@@ -34,3 +34,27 @@ urlArr.forEach((item) => {
     });
   }
 });
+
+// export const urlArr = [
+//   // registryUser
+//   { url: "/registryUserBEGetAll", model: RegistryUserModel },
+//   { url: "/registryUserBEGetOne/:id", model: RegistryUserModel },
+//   { url: "/registryUserBEPostCustom", model: RegistryUserModel },
+//   //confirmUser
+//   { url: "/confirmUserBEGetAll", model: ConfirmedUserModel },
+// ];
+
+// urlArr.forEach((item) => {
+//   if (item.url.includes("GetAll")) {
+//     routes.get(item.url, (rq, rs) => getAll(rq, rs, item.model));
+//   }
+//   if (item.url.includes("GetOne")) {
+//     routes.get(item.url, (rq, rs) => getOne(rq, rs, item.model));
+//   }
+//   if (item.url.includes("registryUserBEPostCustom")) {
+//     routes.post(item.url, (rq, rs) => {
+//       upload(userImgFolName, userImgImgName).single("image"),
+//         registryUserBEPostCustom(rq, rs, item.model, userImgFolName);
+//     });
+//   }
+// });
